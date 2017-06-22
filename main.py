1a35109a539dc8672c01cf89c8fd7e6fe1a8d244
Silvia <surtle@me.com>
2017-06-21 20:33:05 -0700

    added goodbye function

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ app = Flask("peacefulravine")
 @app.route("/")
 def hello():
 	return "Hello World!"
+
+def goodbye():
+        print "Goodbye"
+
+if __name__ == "__main__":
+
+        try:
+                goodbye()
+
+        except KeyboardInterrupt:
+                raise
+
+        
+                
