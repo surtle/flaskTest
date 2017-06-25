4740bc63c5d7a86b1f1212dde2b3998947f3a9e2
Silvia <surtle@me.com>
2017-06-25 13:04:08 -0700

    added db create, migrate, upgrade, downgrade

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
 
+# initialize app
 app = Flask(__name__)
 app.config.from_object('config')
+
+# initialize database
+db = SQLAlchemy(app)
 
 from app import views
