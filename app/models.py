083d95bf9b627ca1d0df7c4b5b0a940526569421
Silvia <surtle@me.com>
2017-06-26 14:01:46 -0700

    finalized primitive database

--- app/models.py
+++ app/models.py
@@ -1,9 +1,48 @@
 from app import db
 
+#====================================================================
+# id - unique ID for the user
+# nickname - unique username for the user
+# email - user's email
+# totalVal - the total amount of money recorded
+# categories - the separate categories the user has specified
+#====================================================================
+
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   nickname = db.Column(db.String(64), index=True, unique=True) 
   email = db.Column(db.String(120), index=True, unique=True) 
+  totVal = db.Column(db.Float) 
+  categories = db.relationship('Category', backref=db.backref('user',
+        lazy='joined'), lazy='dynamic')
+
+  def __init__(self, nickname, email, totVal):
+    self.nickname = nickname
+    self.email = email
+    self.totVal = totVal
 
   def __repr__(self):
     return '<User %r>' % (self.nickname) 
+
+#====================================================================
+# id - unique id for Category 
+# label - category's label
+# value - the amount of money recorded for this category
+# userID - id field that links back to user
+#====================================================================
+
+class Category(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  label = db.Column(db.String(64))
+  value = db.Column(db.Float)
+  userID = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+  def __init__(self, label, value, user):
+    self.label = label
+    self.value = value
+    self.userID = user.id
+
+  def __repr__(self):
+    return '<Category %r>' % (self.label)
+
+
